Fixed bug in text attention mask (mask and padding misaligned), also added additional debug statements throughout and in Original Code

OriginalCode/Multi-Task/source/models/attention.py
@@ -1,8 +1,8 @@
 import torch
 from torch import nn
 
-debug = False
-
+# added version with debugging to compare my training loop with theirs
+# as far as I can tell everything in code looks same (may be issue in dataloader)
 
 class Attention(nn.Module):
     def __init__(self, dimension):
@@ -14,15 +14,72 @@ class Attention(nn.Module):
         self.softmax = nn.Softmax(dim=1)
         self.epsilon = 1e-10
 
-    def forward(self, h, mask):
-       u_it = self.u(h)
-       u_it = self.tanh(u_it)
+    def forward(self, h, mask, debug=False):
+        # Temporary debug variable
+        def dprint(text):
+            if debug:
+                print(text)
+
+        # temporary helper to debug NaNs
+        def dprint_nan_percentage(tensor, tensor_name="tensor"):
+            if debug == True:
+                num_elements = tensor.numel()
+                num_nans = torch.isnan(tensor).sum().item()
+                nan_percentage = (num_nans / num_elements) * 100
+                print(f"{tensor_name} contains {nan_percentage:.2f}% NaNs")
+
+        # batch_size by max sequence length (500 text) by embedding dim (768)
+        dprint(f"Self Attention Input Shape: {h.shape}, Mask Shape {mask.shape}")
+        u_it = self.u(h)
+        u_it = self.tanh(u_it)
+
+        dprint_nan_percentage(u_it, "Linear + Tanh Input")
+        
+        alpha = torch.exp(torch.matmul(u_it, self.v))
+        dprint_nan_percentage(alpha, "Input Multiplied by Values Exponent")
+        dprint(f"Alpha initial shape: {alpha.shape}")
+
+        alpha = mask * alpha + self.epsilon
+        dprint_nan_percentage(alpha, "Applying above to mask")
+        dprint(f"Alpha on Mask Shape: {alpha.shape}")
+        # both alphas will be batch size by max sequence length
+
+        # this is batch_size by 1, and is this source of NaNs -> it quickly explodes
+        # even on first batch this is a very large number
+        denominator_sum = torch.sum(alpha, dim=-1, keepdim=True)
+        dprint(f"Denominator: {denominator_sum}")
+
+        alpha = mask * (alpha / denominator_sum)
+        output = h * alpha.unsqueeze(2)
+        dprint_nan_percentage(output, "Temp Output")
+        dprint(f"Temp Output Shape: {output.shape}")
+        # the shape here is batch size by max sequence length by embedding dimension again
+
+        output = torch.sum(output, dim=1)
+        dprint_nan_percentage(output, "Final Output")
+        dprint(f"Final Output Shape: {output.shape}")
+        # the final output is just batch size by embedding dimension
+        return output, alpha
+
+# class Attention(nn.Module):
+#     def __init__(self, dimension):
+#         super(Attention, self).__init__()
+
+#         self.u = nn.Linear(dimension, dimension)
+#         self.v = nn.Parameter(torch.rand(dimension), requires_grad=True)
+#         self.tanh = nn.Tanh()
+#         self.softmax = nn.Softmax(dim=1)
+#         self.epsilon = 1e-10
+
+#     def forward(self, h, mask):
+#        u_it = self.u(h)
+#        u_it = self.tanh(u_it)
        
-       alpha = torch.exp(torch.matmul(u_it, self.v))
-       alpha = mask * alpha + self.epsilon
-       denominator_sum = torch.sum(alpha, dim=-1, keepdim=True)
-       alpha = mask * (alpha / denominator_sum)
-       output = h * alpha.unsqueeze(2)
-       output = torch.sum(output, dim=1)
-       return output, alpha
+#        alpha = torch.exp(torch.matmul(u_it, self.v))
+#        alpha = mask * alpha + self.epsilon
+#        denominator_sum = torch.sum(alpha, dim=-1, keepdim=True)
+#        alpha = mask * (alpha / denominator_sum)
+#        output = h * alpha.unsqueeze(2)
+#        output = torch.sum(output, dim=1)
+#        return output, alpha
 

finetuning_dataloader.py
@@ -5,7 +5,13 @@ from torch.utils.data import Dataset, DataLoader
 import torch.nn.functional as F
 import time
 import json
-from helpers import pad_segment, mask_vector, pad_features
+from helpers import pad_segment, mask_vector, pad_features, mask_vector_reverse
+
+debug = False
+
+def dprint(text):
+    if debug:
+        print(text)
 
 # I think there's issues with the padding and masking (also doesn't padding set to -infinity)
 
@@ -70,17 +76,17 @@ class CustomDataset(Dataset):
                 masked_img = mask_vector(self.img_pad_length, image_vec)
                 image_vec = pad_segment(image_vec, self.img_pad_length)
             except:
-                print("Image not found")
+                dprint("Image not found")
                 image_vec = torch.zeros((self.img_pad_length, 1024)) 
                 masked_img = torch.zeros(self.img_pad_length)
         else:
-            print("Image not found")
+            dprint("Image not found")
             image_vec = torch.zeros((self.img_pad_length, 1024)) 
             masked_img = torch.zeros(self.img_pad_length)
 
-        print("PADDED IMAGE VEC")
-        print(image_vec)
-        print("IMAGE Mask", masked_img)
+        dprint("PADDED IMAGE VEC")
+        dprint(image_vec)
+        dprint(f"IMAGE Masked {masked_img}")
 
         # Load audio features
         audio_path = os.path.join(self.base_dir, "i3D-vggish-features/vgg_vecs_extended_merged/")
@@ -90,28 +96,27 @@ class CustomDataset(Dataset):
             audio_vec = np.load(audio_path)
             audio_vec = torch.tensor(audio_vec)
         except FileNotFoundError:
-            print("Audio not found")
+            dprint("Audio not found")
             audio_vec = torch.zeros((1, 128))
 
         masked_audio = mask_vector(self.audio_pad_length, audio_vec)
         audio_vec = pad_segment(audio_vec, self.audio_pad_length)
 
-        print("PADDED AUDIO VEC")
-        print(audio_vec)
-        print("AUDIO Mask", masked_audio)
+        dprint("PADDED AUDIO VEC")
+        dprint(audio_vec)
+        dprint(f"AUDIO Mask {masked_audio}")
 
         # Process text
         text = torch.tensor(item['indexes']) # tokenized text
-        # print("TEXT", text)
-        # print("TEXT SHAPE", text.shape)
+        dprint(f"TEXT {text}")
+        dprint(f"TEXT SHAPE {text.shape}")
 
-        mask = mask_vector(self.text_pad_length, text)
-        # print("TEXT MASK", mask)
+        text_mask = mask_vector_reverse(self.text_pad_length, text)
+        dprint(f"TEXT MASK {text_mask}")
 
         # NOTE: PADDING AT BEGINNING FOR SOME REASON
-        # To use my function where you pad at end, go to helpers and uncomment, and feed in text not [text]
         text = pad_features([text], self.text_pad_length)[0]
-        # print("PADDED TEXT", text)
+        dprint(f"PADDED TEXT {text}")
 
         binary = torch.tensor(item['y']) 
         mature = torch.tensor(item["mature"])
@@ -121,7 +126,7 @@ class CustomDataset(Dataset):
 
         return {
             'text': text,
-            'text_mask': mask,
+            'text_mask': text_mask,
             'image': image_vec.float(),
             'image_mask': masked_img,
             'audio': audio_vec.float(),